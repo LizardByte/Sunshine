944ab4b786c93f1c871de430bff705042c077e2d
loki <loki@fakeemail.com>
2020-01-23 11:52:08 +0100

    Remove unnecessary function

--- sunshine/platform/windows.cpp
+++ sunshine/platform/windows.cpp
@@ -276,29 +276,6 @@ void gamepad(input_t &input, const gamepad_state_t &gamepad_state) {
   }
 }
 
-namespace gp {
-void dpad_y(input_t &input, int button_state) {} // up pressed == -1, down pressed == 1, else 0
-void dpad_x(input_t &input, int button_state) {} // left pressed == -1, right pressed == 1, else 0
-void start(input_t &input, int button_down) {}
-void back(input_t &input, int button_down) {}
-void left_stick(input_t &input, int button_down) {}
-void right_stick(input_t &input, int button_down) {}
-void left_button(input_t &input, int button_down) {}
-void right_button(input_t &input, int button_down) {}
-void home(input_t &input, int button_down) {}
-void a(input_t &input, int button_down) {}
-void b(input_t &input, int button_down) {}
-void x(input_t &input, int button_down) {}
-void y(input_t &input, int button_down) {}
-void left_trigger(input_t &input, std::uint8_t abs_z) {}
-void right_trigger(input_t &input, std::uint8_t abs_z) {}
-void left_stick_x(input_t &input, std::int16_t x) {}
-void left_stick_y(input_t &input, std::int16_t y) {}
-void right_stick_x(input_t &input, std::int16_t x) {}
-void right_stick_y(input_t &input, std::int16_t y) {}
-void sync(input_t &input) {}
-}
-
 void freeInput(void *p) {
   auto vigem = (vigem_t*)p;
 
