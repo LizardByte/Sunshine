9e03cdda42587a4325c3b3efa2da34cc02344f44
loki <loki@fakeemail.com>
2021-06-12 18:30:24 +0200

    constant bitrate for audio

--- sunshine/audio.cpp
+++ sunshine/audio.cpp
@@ -85,6 +85,8 @@ void encodeThread(packet_queue_t packets, sample_queue_t samples, config_t confi
     OPUS_APPLICATION_AUDIO,
     nullptr) };
 
+  opus_multistream_encoder_ctl(opus.get(), OPUS_SET_VBR(0));
+
   auto frame_size = config.packetDuration * stream->sampleRate / 1000;
   while(auto sample = samples->pop()) {
     packet_t packet { 1024 }; // 1KB
