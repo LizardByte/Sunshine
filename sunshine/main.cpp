3d6611fd50d1eb95732ab6f4306cd57a572ab735
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2022-04-28 22:51:02 -0400

    Update help argument

--- sunshine/main.cpp
+++ sunshine/main.cpp
@@ -61,17 +61,19 @@ void print_help(const char *name) {
   std::cout
     << "Usage: "sv << name << " [options] [/path/to/configuration_file] [--cmd]"sv << std::endl
     << "    Any configurable option can be overwritten with: \"name=value\""sv << std::endl
+    << std:endl
+    << "    Note: The configuration will be created if it doesn't exist."sv << std::endl
     << std::endl
     << "    --help                    | print help"sv << std::endl
-    << "    --creds username password | set user credentials for the Web manager" << std::endl
-    << "    --version                 | print the version of sunshine" << std::endl
+    << "    --creds username password | set user credentials for the Web manager"sv << std::endl
+    << "    --version                 | print the version of sunshine"sv << std::endl
     << std::endl
     << "    flags"sv << std::endl
     << "        -0 | Read PIN from stdin"sv << std::endl
     << "        -1 | Do not load previously saved state and do retain any state after shutdown"sv << std::endl
     << "           | Effectively starting as if for the first time without overwriting any pairings with your devices"sv << std::endl
-    << "        -2 | Force replacement of headers in video stream" << std::endl
-    << "        -p | Enable/Disable UPnP" << std::endl
+    << "        -2 | Force replacement of headers in video stream"sv << std::endl
+    << "        -p | Enable/Disable UPnP"sv << std::endl
     << std::endl;
 }
 
