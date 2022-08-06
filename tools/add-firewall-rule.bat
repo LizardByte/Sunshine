65e9813b954132fc087b66d321441b3fbfdc3c42
Ryan Caezar Itang <sitiom@proton.me>
2022-08-06 11:03:30 +0800

    Rename rule to Sunshine and limit protocols

--- tools/add-firewall-rule.bat
+++ tools/add-firewall-rule.bat
@@ -1,7 +1,8 @@
 @echo off
 
-set RULE_NAME=SunshineStream
+set RULE_NAME=Sunshine
 set PROGRAM_BIN="%~dp0sunshine.exe"
 
 rem Add the rule
-netsh advfirewall firewall add rule name=%RULE_NAME% dir=in action=allow program=%PROGRAM_BIN% enable=yes
+netsh advfirewall firewall add rule name=%RULE_NAME% dir=in action=allow protocol=tcp program=%PROGRAM_BIN% enable=yes
+netsh advfirewall firewall add rule name=%RULE_NAME% dir=in action=allow protocol=udp program=%PROGRAM_BIN% enable=yes
