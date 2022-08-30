2298cbbe209ce8fc97f042cd83b10c88c14b7702
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2022-08-30 12:39:12 -0400

    remove debugging commands from `add-firewall-rule.bat`

--- src_assets/windows/misc/firewall/add-firewall-rule.bat
+++ src_assets/windows/misc/firewall/add-firewall-rule.bat
@@ -6,10 +6,6 @@ for %%I in ("%~dp0\..") do set "ROOT_DIR=%%~fI"
 set RULE_NAME=Sunshine
 set PROGRAM_BIN="%ROOT_DIR%\sunshine.exe"
 
-echo %PROGRAM_BIN%
-
-pause
-
 rem Add the rule
 netsh advfirewall firewall add rule name=%RULE_NAME% dir=in action=allow protocol=tcp program=%PROGRAM_BIN% enable=yes
 netsh advfirewall firewall add rule name=%RULE_NAME% dir=in action=allow protocol=udp program=%PROGRAM_BIN% enable=yes
