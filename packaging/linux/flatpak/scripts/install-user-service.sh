d222c461abcb920fbbfa82a3c707342b9128591e
istori1 <107304850+istori1@users.noreply.github.com>
2022-11-04 19:26:48 -0400

    Update install-user-service.sh

--- packaging/linux/flatpak/scripts/install-user-service.sh
+++ packaging/linux/flatpak/scripts/install-user-service.sh
@@ -3,5 +3,4 @@
 mkdir -p ~/.config/systemd
 mkdir -p ~/.config/systemd/user
 cp /app/share/sunshine/systemd/user/sunshine.service $HOME/.config/systemd/user/sunshine.service
-sed -i 's/\/app\/bin\/sunshine/flatpak run dev.lizardbyte.sunshine\nExecStop=flatpak kill dev.lizardbyte.sunshine/g' $HOME/.config/systemd/user/sunshine.service
 echo Sunshine User Service has been installed.
