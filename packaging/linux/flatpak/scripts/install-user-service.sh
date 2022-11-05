0930751c8bce8cae8fb0beb2c78ceb097e37ecb2
istori1 <107304850+istori1@users.noreply.github.com>
2022-11-05 06:50:08 -0400

    Update install-user-service.sh

--- packaging/linux/flatpak/scripts/install-user-service.sh
+++ packaging/linux/flatpak/scripts/install-user-service.sh
@@ -4,3 +4,5 @@ mkdir -p ~/.config/systemd
 mkdir -p ~/.config/systemd/user
 cp /app/share/sunshine/systemd/user/sunshine.service $HOME/.config/systemd/user/sunshine.service
 echo Sunshine User Service has been installed.
+echo Use [systemctl --user start sunshine] to start the Sunshine service.
+echo Use [systemctl --user enable sunshine] to autostart the service on login.
