fe46ef173307def63b182a6b4474124a791eb704
Brad Richardson <bradrichardsonmail@gmail.com>
2023-01-02 19:42:00 -0500

    Handle id collision

--- src/confighttp.cpp
+++ src/confighttp.cpp
@@ -302,6 +302,11 @@ void saveApp(resp_https_t response, req_https_t request) {
     response->write(data.str());
   });
 
+  std::set<std::string> ids;
+  for (auto const &app : proc::proc.get_apps()) {
+    ids.insert(app.id);
+  }
+
   pt::ptree inputTree, fileTree;
 
   BOOST_LOG(fatal) << config::stream.file_apps;
@@ -311,8 +316,12 @@ void saveApp(resp_https_t response, req_https_t request) {
     pt::read_json(config::stream.file_apps, fileTree);
 
     // Moonlight checks the id of an item to determine if an item was changed
-    // Needs to be 32-bit positive, signed integer due to client limitations (min of 1 as "0" indicates no app running)
-    inputTree.put("id", util::generate_int32(1, std::numeric_limits<std::int32_t>::max()));
+    // Needs to be a 32-bit positive integer due to client limitations, "0" indicates no app
+    auto id = util::generate_int32(1, std::numeric_limits<std::int32_t>::max());
+    while (ids.count(std::to_string(id)) > 0) {
+      id = util::generate_int32(1, std::numeric_limits<std::int32_t>::max());
+    }
+    inputTree.put("id", id);
 
     if(inputTree.get_child("prep-cmd").empty()) {
       inputTree.erase("prep-cmd");
