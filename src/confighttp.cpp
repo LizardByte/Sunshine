713428b77c9d42c373b07742674cccb4229a073d
Jariullah <safijari@isu.edu>
2023-01-24 12:45:26 -0600

    PR comments

--- src/confighttp.cpp
+++ src/confighttp.cpp
@@ -259,8 +259,12 @@ void getNodeModules(resp_https_t response, req_https_t request) {
   auto filePath = fs::weakly_canonical(webDirPath / request->path);
 
   // Don't do anything if file does not exist or is outside the node_modules directory
-  if (!isChildPath(filePath, nodeModulesPath) || !fs::exists(filePath)) {
+  if (!fs::exists(filePath)) {
     response->write(SimpleWeb::StatusCode::client_error_not_found);
+    
+  } else if (!isChildPath(filePath, nodeModulesPath)) {
+    BOOST_LOG(warning) << "Someone requested a path " << filePath << " that is outside the node_modules folder";
+    response->write(SimpleWeb::StatusCode::client_error_bad_request, "Not Authorized");
   } else {
     auto relPath = fs::relative(filePath, webDirPath);
     if (relPath.extension() == ".ttf" or relPath.extension() == ".woff2") {
