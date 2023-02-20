27b49bb6b3950edf74dc9b24de59e6eadfbd927b
Brian Kendall <brian@briankendall.net>
2023-02-19 22:58:18 -0500

    Support compiling for earlier releases of macOS
    Allows compiling for macOS 10.12 and later

--- src/platform/macos/misc.mm
+++ src/platform/macos/misc.mm
@@ -1,3 +1,4 @@
+#include <Foundation/Foundation.h>
 #include <arpa/inet.h>
 #include <dlfcn.h>
 #include <fcntl.h>
@@ -17,13 +18,40 @@ namespace bp = boost::process;
 
 namespace platf {
 
+// Even though the following two functions are available starting in macOS 10.15, they weren't
+// actually in the Mac SDK until Xcode 12.2, the first to include the SDK for macOS 11
+#if __MAC_OS_X_VERSION_MAX_ALLOWED < 110000 // __MAC_11_0
+// If they're not in the SDK then we can use our own function definitions.
+// Need to use weak import so that this will link in macOS 10.14 and earlier
+extern "C" bool CGPreflightScreenCaptureAccess(void) __attribute__((weak_import));
+extern "C" bool CGRequestScreenCaptureAccess(void) __attribute__((weak_import));
+#endif
+
 std::unique_ptr<deinit_t> init() {
-  if(!CGPreflightScreenCaptureAccess()) {
+  // This will generate a warning about CGPreflightScreenCaptureAccess and
+  // CGRequestScreenCaptureAccess being unavailable before macOS 10.15, but
+  // we have a guard to prevent it from being called on those earlier systems.
+  // Unfortunately the supported way to silence this warning, using @available,
+  // produces linker errors for __isPlatformVersionAtLeast, so we have to use
+  // a different method.
+  // We also ignore "tautological-pointer-compare" because when compiling with
+  // Xcode 12.2 and later, these functions are not weakly linked and will never
+  // be null, and therefore generate this warning. Since we are weakly linking
+  // when compiling with earlier Xcode versions, the check for null is
+  // necessary and so we ignore the warning.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunguarded-availability-new"
+#pragma clang diagnostic ignored "-Wtautological-pointer-compare"
+  if([[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion:((NSOperatingSystemVersion) { 10, 15, 0 })] &&
+     // Double check that these weakly-linked symbols have been loaded:
+     CGPreflightScreenCaptureAccess != nullptr && CGRequestScreenCaptureAccess != nullptr &&
+     !CGPreflightScreenCaptureAccess()) {
     BOOST_LOG(error) << "No screen capture permission!"sv;
     BOOST_LOG(error) << "Please activate it in 'System Preferences' -> 'Privacy' -> 'Screen Recording'"sv;
     CGRequestScreenCaptureAccess();
     return nullptr;
   }
+#pragma clang diagnostic pop
   return std::make_unique<deinit_t>();
 }
 
