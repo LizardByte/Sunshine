ad464b14eecc49cb04e6a03675a44537a1e403cb
jacob.a.speer@gmail.com <jacob.a.speer@gmail.com>
2023-03-12 13:08:37 -0500

    Re-working the dark theme selector icon and defaulting to auto

--- src_assets/common/assets/web/js/main.js
+++ src_assets/common/assets/web/js/main.js
@@ -1,66 +1,79 @@
 var sunshineUI = {
-  init: function(){
+  init: function() {
     sunshineUI.toggleTheme();
-    sunshineUI.getCurrentPage();
+    //document click events
+    const btn = document.querySelector("#toggleTheme");
+    btn.addEventListener("click", function() {
+      sunshineUI.toggleTheme(1);
+    });
 
   },
-  getCurrentPage: function(){
-      let el = document.querySelector("a[href='" + document.location.pathname + "']");
-      if (el) el.classList.add("active")
-  },
-	toggleTheme: function (btn) {
+  toggleTheme: function(click) {
     let themeCookie = getCookie("sunshineTheme");
-        
-    if(!btn){
+    let options = document.querySelector('#toggleTheme');
+    let btn = options.querySelector('.active');
+
+    if (!click) {
       //init page load
-      let options = document.querySelector('#toggleTheme .dropdown-menu');
-      if(themeCookie == null){
-        //no cookie, get the active button
-        btn = options.querySelector('.dropdown-item.active');
-      }else{
+      if (themeCookie != null) {
         //cookie is set, make that button active
-        btn = options.querySelectorAll('[data-bs-theme="'+themeCookie+'"]')[0];
+        btn = options.querySelectorAll('[data-bs-theme="' + themeCookie + '"]')[0];
       }
+    } else {
+      let optionCount = getChildNodes(btn).count;
+      let optionIndex = getChildNodes(btn).index;
+      if (optionIndex < (optionCount - 1)) {
+        optionIndex++;
+      } else {
+        optionIndex = 0;
+      }
+      btn = options.children[optionIndex];
     }
     //set the theme & cookie
     let theme = btn.getAttribute('data-bs-theme');
-    document.cookie = 'sunshineTheme='+theme+';';
-    
+    document.cookie = 'sunshineTheme=' + theme + ';';
+
     //remove all active classes from all buttons
-    btn.closest('.dropdown-menu').querySelectorAll('button').forEach((element) => {
+    options.querySelectorAll('span').forEach((element) => {
       element.classList.remove('active');
     });
-    
+
+
     //If it's the auto theme, determine what the computer prefers
-    if(theme == "auto"){
+    if (theme == "auto") {
       if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         theme = "dark";
-      }else{
+      } else {
         theme = "light";
       }
     }
-    
+
     //apply data attribute to html
     document.querySelector('html').setAttribute('data-bs-theme', theme);
-    let icon = document.querySelector("#toggleThemeMenu i");
-    icon.className = btn.querySelector('i').className;
+    let icon = document.querySelector("#toggleTheme .active i");
+    //icon.className = btn.query.className;
     btn.classList.add("active");
-	}
+  }
 }
-
-//document click events
-document.addEventListener('click', function (event) {
-  //Toggle: Theme picker
-  if (event.target.matches('#toggleTheme .dropdown-menu .dropdown-item')){
-    sunshineUI.toggleTheme(event.target);
-  };
-
-}, false);
 
 sunshineUI.init();
 
-function getCookie (name) {
-	let value = `; ${document.cookie}`;
-	let parts = value.split(`; ${name}=`);
-	if (parts.length === 2) return parts.pop().split(';').shift();
+function getCookie(name) {
+  let value = `; ${document.cookie}`;
+  let parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function getChildNodes(elm) {
+  var obj = {
+    children: elm.parentNode.children,
+    count: elm.parentNode.children.length
+  }
+  var c = obj.count;
+  while (c--) {
+    if (obj.children[c] == elm) {
+      obj.index = (c);
+    }
+  }
+  return obj;
 }
