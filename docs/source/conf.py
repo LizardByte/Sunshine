4ddee255d8f4ca30353cac219509e3ad971d3020
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2023-03-15 21:40:51 -0400

    docs: add favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,12 +67,14 @@ source_suffix = ['.rst', '.md']
 
 # -- Options for HTML output -------------------------------------------------
 
+# images
+html_favicon = os.path.join(root_dir, 'src_assets', 'common', 'assets', 'web', 'images', 'favicon.ico')
+html_logo = os.path.join(root_dir, 'sunshine.png')
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
-
-html_logo = os.path.join(root_dir, 'sunshine.png')
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
