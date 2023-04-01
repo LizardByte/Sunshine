5d3034701d9e8e73aba4527fa6e11af2a11ade1e
kodemeister <kodemeister@outlook.com>
2023-04-02 03:48:24 +0600

    Remove setup of 'input' group from Linux packaging scripts

--- packaging/linux/flatpak/scripts/remove-additional-install.sh
+++ packaging/linux/flatpak/scripts/remove-additional-install.sh
@@ -6,6 +6,6 @@ rm $HOME/.config/systemd/user/sunshine.service
 systemctl --user daemon-reload
 echo Sunshine User Service has been removed.
 
-# Udev rule and input group
-flatpak-spawn --host pkexec sh -c "gpasswd -d $USER input && rm /etc/udev/rules.d/85-sunshine.rules"
+# Udev rule
+flatpak-spawn --host pkexec sh -c "rm /etc/udev/rules.d/85-sunshine.rules"
 echo Mouse permission removed. Restart computer to take effect.
