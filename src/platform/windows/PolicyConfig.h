08b2cbb3f6b7c5a92be84b769a697cdc12f298d7
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2023-04-28 10:57:51 -0400

    fix(audio)!: remove policy config for Windows Vista

--- src/platform/windows/PolicyConfig.h
+++ src/platform/windows/PolicyConfig.h
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include <mmdeviceapi.h>
+
 #ifdef __MINGW32__
   #undef DEFINE_GUID
   #ifdef __cplusplus
@@ -99,81 +101,3 @@ public:
     PCWSTR,
     INT);
 };
-
-interface DECLSPEC_UUID("568b9108-44bf-40b4-9006-86afe5b5a620") IPolicyConfigVista;
-class DECLSPEC_UUID("294935CE-F637-4E7C-A41B-AB255460B862") CPolicyConfigVistaClient;
-// ----------------------------------------------------------------------------
-// class CPolicyConfigVistaClient
-// {294935CE-F637-4E7C-A41B-AB255460B862}
-//
-// interface IPolicyConfigVista
-// {568b9108-44bf-40b4-9006-86afe5b5a620}
-//
-// Query interface:
-// CComPtr<IPolicyConfigVista> PolicyConfig;
-// PolicyConfig.CoCreateInstance(__uuidof(CPolicyConfigVistaClient));
-//
-// @compatible: Windows Vista and Later
-// ----------------------------------------------------------------------------
-interface IPolicyConfigVista: public IUnknown {
-public:
-  virtual HRESULT
-  GetMixFormat(
-    PCWSTR,
-    WAVEFORMATEX **);  // not available on Windows 7, use method from IPolicyConfig
-
-  virtual HRESULT STDMETHODCALLTYPE
-  GetDeviceFormat(
-    PCWSTR,
-    INT,
-    WAVEFORMATEX **);
-
-  virtual HRESULT STDMETHODCALLTYPE
-  SetDeviceFormat(
-    PCWSTR,
-    WAVEFORMATEX *,
-    WAVEFORMATEX *);
-
-  virtual HRESULT STDMETHODCALLTYPE GetProcessingPeriod(
-    PCWSTR,
-    INT,
-    PINT64,
-    PINT64);  // not available on Windows 7, use method from IPolicyConfig
-
-  virtual HRESULT STDMETHODCALLTYPE SetProcessingPeriod(
-    PCWSTR,
-    PINT64);  // not available on Windows 7, use method from IPolicyConfig
-
-  virtual HRESULT STDMETHODCALLTYPE
-  GetShareMode(
-    PCWSTR,
-    struct DeviceShareMode *);  // not available on Windows 7, use method from IPolicyConfig
-
-  virtual HRESULT STDMETHODCALLTYPE
-  SetShareMode(
-    PCWSTR,
-    struct DeviceShareMode *);  // not available on Windows 7, use method from IPolicyConfig
-
-  virtual HRESULT STDMETHODCALLTYPE
-  GetPropertyValue(
-    PCWSTR,
-    const PROPERTYKEY &,
-    PROPVARIANT *);
-
-  virtual HRESULT STDMETHODCALLTYPE
-  SetPropertyValue(
-    PCWSTR,
-    const PROPERTYKEY &,
-    PROPVARIANT *);
-
-  virtual HRESULT STDMETHODCALLTYPE
-  SetDefaultEndpoint(
-    PCWSTR wszDeviceId,
-    ERole eRole);
-
-  virtual HRESULT STDMETHODCALLTYPE SetEndpointVisibility(
-    PCWSTR,
-    INT);  // not available on Windows 7, use method from IPolicyConfig
-};
-
-// ----------------------------------------------------------------------------
