ce714c8588e658700deb7e20fe9f643d30db3201
Cameron Gutman <aicommander@gmail.com>
2023-06-25 17:35:56 -0500

    Implement extended controller button support

--- src/platform/common.h
+++ src/platform/common.h
@@ -49,21 +49,27 @@ namespace video {
 namespace platf {
   constexpr auto MAX_GAMEPADS = 32;
 
-  constexpr std::uint16_t DPAD_UP = 0x0001;
-  constexpr std::uint16_t DPAD_DOWN = 0x0002;
-  constexpr std::uint16_t DPAD_LEFT = 0x0004;
-  constexpr std::uint16_t DPAD_RIGHT = 0x0008;
-  constexpr std::uint16_t START = 0x0010;
-  constexpr std::uint16_t BACK = 0x0020;
-  constexpr std::uint16_t LEFT_STICK = 0x0040;
-  constexpr std::uint16_t RIGHT_STICK = 0x0080;
-  constexpr std::uint16_t LEFT_BUTTON = 0x0100;
-  constexpr std::uint16_t RIGHT_BUTTON = 0x0200;
-  constexpr std::uint16_t HOME = 0x0400;
-  constexpr std::uint16_t A = 0x1000;
-  constexpr std::uint16_t B = 0x2000;
-  constexpr std::uint16_t X = 0x4000;
-  constexpr std::uint16_t Y = 0x8000;
+  constexpr std::uint32_t DPAD_UP = 0x0001;
+  constexpr std::uint32_t DPAD_DOWN = 0x0002;
+  constexpr std::uint32_t DPAD_LEFT = 0x0004;
+  constexpr std::uint32_t DPAD_RIGHT = 0x0008;
+  constexpr std::uint32_t START = 0x0010;
+  constexpr std::uint32_t BACK = 0x0020;
+  constexpr std::uint32_t LEFT_STICK = 0x0040;
+  constexpr std::uint32_t RIGHT_STICK = 0x0080;
+  constexpr std::uint32_t LEFT_BUTTON = 0x0100;
+  constexpr std::uint32_t RIGHT_BUTTON = 0x0200;
+  constexpr std::uint32_t HOME = 0x0400;
+  constexpr std::uint32_t A = 0x1000;
+  constexpr std::uint32_t B = 0x2000;
+  constexpr std::uint32_t X = 0x4000;
+  constexpr std::uint32_t Y = 0x8000;
+  constexpr std::uint32_t PADDLE1 = 0x010000;
+  constexpr std::uint32_t PADDLE2 = 0x020000;
+  constexpr std::uint32_t PADDLE3 = 0x040000;
+  constexpr std::uint32_t PADDLE4 = 0x080000;
+  constexpr std::uint32_t TOUCHPAD_BUTTON = 0x100000;
+  constexpr std::uint32_t MISC_BUTTON = 0x200000;
 
   struct rumble_t {
     KITTY_DEFAULT_CONSTR(rumble_t)
@@ -154,7 +160,7 @@ namespace platf {
   };
 
   struct gamepad_state_t {
-    std::uint16_t buttonFlags;
+    std::uint32_t buttonFlags;
     std::uint8_t lt;
     std::uint8_t rt;
     std::int16_t lsX;
