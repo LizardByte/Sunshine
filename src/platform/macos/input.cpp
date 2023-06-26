3d8500953c333f1c2ea1eea11ca0be840915c82c
Cameron Gutman <aicommander@gmail.com>
2023-06-25 20:16:26 -0500

    Implement controller arrival metadata support

--- src/platform/macos/input.cpp
+++ src/platform/macos/input.cpp
@@ -288,8 +288,16 @@ const KeyCodeMap kKeyCodesMap[] = {
     BOOST_LOG(info) << "unicode: Unicode input not yet implemented for MacOS."sv;
   }
 
+  /**
+   * @brief Creates a new virtual gamepad.
+   * @param input The input context.
+   * @param nr The assigned controller number.
+   * @param metadata Controller metadata from client (empty if none provided).
+   * @param rumble_queue The queue for posting rumble messages to the client.
+   * @return 0 on success.
+   */
   int
-  alloc_gamepad(input_t &input, int nr, rumble_queue_t rumble_queue) {
+  alloc_gamepad(input_t &input, int nr, const gamepad_arrival_t &metadata, rumble_queue_t rumble_queue) {
     BOOST_LOG(info) << "alloc_gamepad: Gamepad not yet implemented for MacOS."sv;
     return -1;
   }
