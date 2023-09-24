7af6580cc15b1c99d3b8349b3b27a3e3dbccf292
Elia Zammuto <theelixzammuto@gmail.com>
2023-09-24 21:57:34 +0200

    Migrated to EJS, app page bugfixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
+import fs from 'fs';
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import { ViteEjsPlugin } from "vite-plugin-ejs";
 import vue from '@vitejs/plugin-vue'
 
+
+/**
+ * Before actually building the pages with Vite, we do an intermediate build step using ejs
+ * Importing this separately and joining them using ejs 
+ * allows us to split some repeating HTML that cannot be added 
+ * by Vue itself (e.g. style/script loading, common meta head tags, Widgetbot)
+ * The vite-plugin-ejs handles this automatically
+ */
+let header = fs.readFileSync(resolve(__dirname, "src_assets/common/assets/web/template_header.html"))
+let headerMain = fs.readFileSync(resolve(__dirname, "src_assets/common/assets/web/template_header_main.html"))
 // https://vitejs.dev/config/
 export default defineConfig({
     resolve: {
@@ -10,7 +22,7 @@ export default defineConfig({
             vue: 'vue/dist/vue.esm-bundler.js'
         }
     },
-    plugins: [vue()],
+    plugins: [vue(), ViteEjsPlugin({ header, headerMain })],
     root: resolve(__dirname, "src_assets/common/assets/web"),
     build: {
         outDir: resolve(__dirname, "build/assets/web"), //TODO Handle SRC_PATH by CMAKE
