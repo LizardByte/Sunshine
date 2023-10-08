9837c94c291e6d7af396a93fc909fc5a6c5c2c3d
ap4ss3rby <100168186+ap4ss3rby@users.noreply.github.com>
2023-10-08 19:07:32 +0300

    Drop any unhandled events and handle LI_TOUCH_EVENT_MOVE/DOWN

--- src/platform/linux/input.cpp
+++ src/platform/linux/input.cpp
@@ -1619,8 +1619,14 @@ namespace platf {
         }
         libevdev_uinput_write_event(touchscreen, EV_SYN, SYN_REPORT, 0);
         return;
+    case LI_TOUCH_EVENT_DOWN:
+          id = touch.pointerId;
+        break;
+    case LI_TOUCH_EVENT_MOVE:
+          id = touch.pointerId;
+        break;
     default:
-        id = touch.pointerId;
+        return;
     }
 
 
