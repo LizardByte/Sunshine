91d25f7fea1ca8ae43b7fa3c856dd9f715eb561e
detiam <dehe_tian@outlook.com>
2023-10-21 08:18:25 +0800

    Update linux .desktop files - Start Sunshine by systemd service when possible. - Update `Categories` and `Keywords`. - Set `Terminal` to false when possible.

--- cmake/packaging/linux.cmake
+++ cmake/packaging/linux.cmake
@@ -88,7 +88,10 @@ endif()
 
 # desktop file
 # todo - validate desktop files with `desktop-file-validate`
-install(FILES "${CMAKE_CURRENT_BINARY_DIR}/sunshine.desktop" DESTINATION "${CMAKE_INSTALL_PREFIX}/share/applications")
+install(FILES "${CMAKE_CURRENT_BINARY_DIR}/sunshine.desktop"
+        DESTINATION "${CMAKE_INSTALL_PREFIX}/share/applications")
+install(FILES "${CMAKE_CURRENT_BINARY_DIR}/sunshine_terminal.desktop"
+        DESTINATION "${CMAKE_INSTALL_PREFIX}/share/applications")
 if(${SUNSHINE_BUILD_FLATPAK})
     install(FILES "${CMAKE_CURRENT_BINARY_DIR}/sunshine_kms.desktop"
             DESTINATION "${CMAKE_INSTALL_PREFIX}/share/applications")
