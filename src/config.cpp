8cbd3f5eacc0b9174dd647bed8451436fc94397b
Erick C <ekchang@users.noreply.github.com>
2023-10-22 01:04:44 -0400

    Update docs and rename config to ds4_back_as_touchpad_click

--- src/config.cpp
+++ src/config.cpp
@@ -1042,7 +1042,7 @@ namespace config {
     }
 
     string_restricted_f(vars, "gamepad"s, input.gamepad, platf::supported_gamepads());
-    bool_f(vars, "ds4_back_as_touchpad", input.ds4_back_as_touchpad);
+    bool_f(vars, "ds4_back_as_touchpad_click", input.ds4_back_as_touchpad_click);
 
     bool_f(vars, "mouse", input.mouse);
     bool_f(vars, "keyboard", input.keyboard);
--- src/platform/windows/input.cpp
+++ src/platform/windows/input.cpp
@@ -1335,8 +1335,8 @@ namespace platf {
     if (gamepad_state.buttonFlags & (TOUCHPAD_BUTTON | MISC_BUTTON)) buttons |= DS4_SPECIAL_BUTTON_TOUCHPAD;
     
     // Manual DS4 emulation: check if BACK button should also trigger DS4 touchpad click
-    if ((config::input.gamepad == "ps4"sv || config::input.gamepad == "ds4"sv) 
-      && config::input.ds4_back_as_touchpad
+    if (config::input.gamepad == "ds4"sv 
+      && config::input.ds4_back_as_touchpad_click
       && (gamepad_state.buttonFlags & BACK)) buttons |= DS4_SPECIAL_BUTTON_TOUCHPAD;
 
     return (DS4_SPECIAL_BUTTONS) buttons;
