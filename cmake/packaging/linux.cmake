2cf76e4cfa761d5995314e82be419d7582cd139b
detiam <dehe_tian@outlook.com>
2023-10-26 01:33:03 +0800

    Fix Appimage build error

--- cmake/packaging/linux.cmake
+++ cmake/packaging/linux.cmake
@@ -90,8 +90,10 @@ endif()
 # todo - validate desktop files with `desktop-file-validate`
 install(FILES "${CMAKE_CURRENT_BINARY_DIR}/sunshine.desktop"
         DESTINATION "${CMAKE_INSTALL_PREFIX}/share/applications")
-install(FILES "${CMAKE_CURRENT_BINARY_DIR}/sunshine_terminal.desktop"
-        DESTINATION "${CMAKE_INSTALL_PREFIX}/share/applications")
+if(NOT ${SUNSHINE_BUILD_APPIMAGE})
+    install(FILES "${CMAKE_CURRENT_BINARY_DIR}/sunshine_terminal.desktop"
+            DESTINATION "${CMAKE_INSTALL_PREFIX}/share/applications")
+endif()
 if(${SUNSHINE_BUILD_FLATPAK})
     install(FILES "${CMAKE_CURRENT_BINARY_DIR}/sunshine_kms.desktop"
             DESTINATION "${CMAKE_INSTALL_PREFIX}/share/applications")
