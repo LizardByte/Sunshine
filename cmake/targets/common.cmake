245d7ef6870c57aab2300965dc663245c6885ba2
Elia Zammuto <theelixzammuto@gmail.com>
2023-12-03 16:28:58 +0100

    Fix cmake lint

--- cmake/targets/common.cmake
+++ cmake/targets/common.cmake
@@ -38,4 +38,4 @@ target_compile_options(sunshine PRIVATE $<$<COMPILE_LANGUAGE:CXX>:${SUNSHINE_COM
 add_custom_target(web-ui ALL 
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} 
         COMMENT "Installing NPM Dependencies and Building the Web UI"
-        COMMAND bash -c \"npm install && SUNSHINE_SOURCE_ASSETS_DIR=${SUNSHINE_SOURCE_ASSETS_DIR} SUNSHINE_ASSETS_DIR=${CMAKE_BINARY_DIR} npm run build\")
+        COMMAND bash -c \"npm install && SUNSHINE_SOURCE_ASSETS_DIR=${SUNSHINE_SOURCE_ASSETS_DIR} SUNSHINE_ASSETS_DIR=${CMAKE_BINARY_DIR} npm run build\") # cmake-lint: disable=C0301
