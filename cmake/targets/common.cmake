dd5d1e835a80b93b0caafd49cc9813e0e26b289c
Elia Zammuto <theelixzammuto@gmail.com>
2023-12-03 16:35:39 +0100

    fix again cmake lint

--- cmake/targets/common.cmake
+++ cmake/targets/common.cmake
@@ -35,7 +35,7 @@ endforeach()
 target_compile_options(sunshine PRIVATE $<$<COMPILE_LANGUAGE:CXX>:${SUNSHINE_COMPILE_OPTIONS}>;$<$<COMPILE_LANGUAGE:CUDA>:${SUNSHINE_COMPILE_OPTIONS_CUDA};-std=c++17>)  # cmake-lint: disable=C0301
 
 #WebUI build
-add_custom_target(web-ui ALL 
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} 
+add_custom_target(web-ui ALL
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         COMMENT "Installing NPM Dependencies and Building the Web UI"
         COMMAND bash -c \"npm install && SUNSHINE_SOURCE_ASSETS_DIR=${SUNSHINE_SOURCE_ASSETS_DIR} SUNSHINE_ASSETS_DIR=${CMAKE_BINARY_DIR} npm run build\") # cmake-lint: disable=C0301
