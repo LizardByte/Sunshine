54c36b80de0742be3874369eda9669afa20bd842
Cameron Gutman <aicommander@gmail.com>
2024-01-04 00:20:14 -0600

    Remove useless texture size queries for every frame in display_ram_t

--- src/platform/linux/wlgrab.cpp
+++ src/platform/linux/wlgrab.cpp
@@ -182,12 +182,6 @@ namespace wl {
       }
 
       gl::ctx.BindTexture(GL_TEXTURE_2D, (*rgb_opt)->tex[0]);
-
-      int w, h;
-      gl::ctx.GetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &w);
-      gl::ctx.GetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &h);
-      BOOST_LOG(debug) << "width and height: w "sv << w << " h "sv << h;
-
       gl::ctx.GetTextureSubImage((*rgb_opt)->tex[0], 0, 0, 0, 0, width, height, 1, GL_BGRA, GL_UNSIGNED_BYTE, img_out->height * img_out->row_pitch, img_out->data);
       gl::ctx.BindTexture(GL_TEXTURE_2D, 0);
 
