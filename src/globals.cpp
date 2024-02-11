2e8d51ec70453c030703dfd92a59d3f02199f6c4
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2024-02-11 09:36:39 -0500

    docs(src): add documentation blocks to globals.cpp

--- src/globals.cpp
+++ src/globals.cpp
@@ -4,6 +4,17 @@
  */
 #include "globals.h"
 
+/**
+ * @brief A process-wide communication mechanism.
+ */
 safe::mail_t mail::man;
+
+/**
+ * @brief A thread pool for processing tasks.
+ */
 thread_pool_util::ThreadPool task_pool;
+
+/**
+ * @brief A boolean flag to indicate whether the cursor should be displayed.
+ */
 bool display_cursor = true;
