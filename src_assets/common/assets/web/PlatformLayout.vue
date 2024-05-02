ffe2298ea0f8cfa811ffa528825c24a99b0fd5a8
Vithorio Polten <reach@vithor.io>
2024-05-02 15:57:27 -0300

    First batch of suggestions from code review
    Co-authored-by: ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>

--- src_assets/common/assets/web/PlatformLayout.vue
+++ src_assets/common/assets/web/PlatformLayout.vue
@@ -1,24 +1,24 @@
 <script setup>
-const props = defineProps({
-  platform: {
-    type: String,
-    required: true
-  }
-})
+  const props = defineProps({
+    platform: {
+      type: String,
+      required: true
+    }
+  })
 
 </script>
 <template>
-<template v-if="$slots.windows && platform === 'windows'">
-  <slot name="windows"></slot>
-</template>
+  <template v-if="$slots.windows && platform === 'windows'">
+    <slot name="windows"></slot>
+  </template>
 
-<template v-if="$slots.linux && platform === 'linux'">
-  <slot name="linux"></slot>
-</template>
+  <template v-if="$slots.linux && platform === 'linux'">
+    <slot name="linux"></slot>
+  </template>
 
-<template v-if="$slots.macos && platform === 'macos'">
-  <slot name="macos"></slot>
-</template>
+  <template v-if="$slots.macos && platform === 'macos'">
+    <slot name="macos"></slot>
+  </template>
 </template>
 
 
