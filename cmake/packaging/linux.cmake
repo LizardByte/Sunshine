d20b3ae43faebd713d03143f079947a044d883ca
Gilles Schintgen <gschintgen@hambier.lu>
2024-07-23 13:42:18 +0200

    packaging/Linux: ensure that uhid is loaded automatically

--- cmake/packaging/linux.cmake
+++ cmake/packaging/linux.cmake
@@ -8,6 +8,8 @@ file(COPY "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/assets/"
 if(${SUNSHINE_BUILD_APPIMAGE} OR ${SUNSHINE_BUILD_FLATPAK})
     install(FILES "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/misc/60-sunshine.rules"
             DESTINATION "${SUNSHINE_ASSETS_DIR}/udev/rules.d")
+    install(FILES "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/misc/60-sunshine.conf"
+            DESTINATION "${SUNSHINE_ASSETS_DIR}/modules-load.d")
     install(FILES "${CMAKE_CURRENT_BINARY_DIR}/sunshine.service"
             DESTINATION "${SUNSHINE_ASSETS_DIR}/systemd/user")
 else()
@@ -21,6 +23,8 @@ else()
     if(SYSTEMD_FOUND)
         install(FILES "${CMAKE_CURRENT_BINARY_DIR}/sunshine.service"
                 DESTINATION "${SYSTEMD_USER_UNIT_INSTALL_DIR}")
+        install(FILES "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/misc/60-sunshine.conf"
+                DESTINATION "${SYSTEMD_MODULES_LOAD_DIR}")
     endif()
 endif()
 
--- packaging/linux/flatpak/scripts/remove-additional-install.sh
+++ packaging/linux/flatpak/scripts/remove-additional-install.sh
@@ -6,6 +6,9 @@ rm $HOME/.config/systemd/user/sunshine.service
 systemctl --user daemon-reload
 echo Sunshine User Service has been removed.
 
+# Uhid loading
+flatpak-spawn --host pkexec sh -c "rm /etc/modules-load.d/60-sunshine.conf"
+
 # Udev rule
 flatpak-spawn --host pkexec sh -c "rm /etc/udev/rules.d/60-sunshine.rules"
 echo Mouse permission removed. Restart computer to take effect.
