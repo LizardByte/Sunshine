a28e1c98fecdd4dee1f65d0c8f69e87f9d79b697
Gilles Schintgen <gschintgen@hambier.lu>
2024-08-15 20:55:35 +0200

    flatpak review suggestion
    Co-authored-by: ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>

--- packaging/linux/flatpak/scripts/remove-additional-install.sh
+++ packaging/linux/flatpak/scripts/remove-additional-install.sh
@@ -6,9 +6,7 @@ rm $HOME/.config/systemd/user/sunshine.service
 systemctl --user daemon-reload
 echo Sunshine User Service has been removed.
 
-# Uhid loading
+# Remove rules
 flatpak-spawn --host pkexec sh -c "rm /etc/modules-load.d/60-sunshine.conf"
-
-# Udev rule
 flatpak-spawn --host pkexec sh -c "rm /etc/udev/rules.d/60-sunshine.rules"
 echo Mouse permission removed. Restart computer to take effect.
