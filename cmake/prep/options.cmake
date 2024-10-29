33107b822fb2d8078947c5dfb4ed41cb4b63ca51
qiin2333 <414382190@qq.com>
2024-10-29 18:37:19 +0800

    update submodules

--- cmake/prep/options.cmake
+++ cmake/prep/options.cmake
@@ -7,9 +7,9 @@ set(SUNSHINE_PUBLISHER_ISSUE_URL "https://app.lizardbyte.dev/support"
         CACHE STRING "The URL of the publisher's support site or issue tracker.
         If you provide a modified version of Sunshine, we kindly request that you use your own url.")
 
-option(BUILD_DOCS "Build documentation" ON)
-option(BUILD_TESTS "Build tests" ON)
-option(TESTS_ENABLE_PYTHON_TESTS "Enable Python tests" ON)
+option(BUILD_DOCS "Build documentation" OFF)
+option(BUILD_TESTS "Build tests" OFF)
+option(TESTS_ENABLE_PYTHON_TESTS "Enable Python tests" OFF)
 
 # DirectX11 is not available in GitHub runners, so even software encoding fails
 set(TESTS_SOFTWARE_ENCODER_UNAVAILABLE "fail"
