8ea99e66c3c7e11150a40cab1ac319002242211d
MiroKaku <50670906+MiroKaku@users.noreply.github.com>
2024-11-25 10:23:00 +0800

    fix(process): proc status lost when streaming

--- src/process.cpp
+++ src/process.cpp
@@ -371,6 +371,23 @@ namespace proc {
   proc_t::get_apps() {
     return _apps;
   }
+  void
+  proc_t::set_apps(std::vector<ctx_t> apps) {
+    _apps = std::move(apps);
+  }
+
+  const boost::process::v1::environment &
+  proc_t::get_env() const {
+    return _env;
+  }
+  boost::process::v1::environment &
+  proc_t::get_env() {
+    return _env;
+  }
+  void
+  proc_t::set_env(boost::process::v1::environment env) {
+    _env = std::move(env);
+  }
 
   // Gets application image from application list.
   // Returns image from assets directory if found there.
@@ -779,7 +796,8 @@ namespace proc {
     auto proc_opt = proc::parse(file_name);
 
     if (proc_opt) {
-      proc = std::move(*proc_opt);
+      proc.set_env(proc_opt->get_env());
+      proc.set_apps(proc_opt->get_apps());
     }
   }
 }  // namespace proc
