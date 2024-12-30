c27dbc0052f8ae05fbe524eba2aade15d5f1c3b2
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2024-12-30 10:28:39 -0500

    fix missing new line

--- src_assets/windows/assets/shaders/directx/include/base_vs.hlsl
+++ src_assets/windows/assets/shaders/directx/include/base_vs.hlsl
@@ -24,8 +24,10 @@ vertex_t generate_fullscreen_triangle_vertex(uint vertex_id, float2 subsample_of
                                      sin(rotation_radians), cos(rotation_radians) };
         float2 rotation_center = { 0.5, 0.5 };
         tex_coord = round(rotation_center + mul(rotation_matrix, tex_coord - rotation_center));
+
         // Swap the xy offset coordinates if the texture is rotated an odd number of times.
         if (rotate_texture_steps & 1) {
+
 
             subsample_offset.xy = subsample_offset.yx;
         }
