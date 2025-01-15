c2b7361e303e2b32927d30dd4cc0822f072e7636
FrogTheFrog <warliukz@gmail.com>
2025-01-15 20:30:29 +0200

    fix(cmake/windows): static link MinHook

--- cmake/dependencies/windows.cmake
+++ cmake/dependencies/windows.cmake
@@ -4,9 +4,9 @@
 find_package(nlohmann_json CONFIG 3.11 REQUIRED)
 
 # Make sure MinHook is installed
-find_library(MINHOOK_LIBRARY minhook REQUIRED)
+find_library(MINHOOK_LIBRARY libMinHook.a REQUIRED)
 find_path(MINHOOK_INCLUDE_DIR MinHook.h PATH_SUFFIXES include REQUIRED)
 
-add_library(minhook::minhook UNKNOWN IMPORTED)
+add_library(minhook::minhook STATIC IMPORTED)
 set_property(TARGET minhook::minhook PROPERTY IMPORTED_LOCATION ${MINHOOK_LIBRARY})
 target_include_directories(minhook::minhook INTERFACE ${MINHOOK_INCLUDE_DIR})
