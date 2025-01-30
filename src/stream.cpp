c5484ba55d8d5c2825e9e7683a29145afd503bd5
Vincent LaRocca <vincentmlarocca@gmail.com>
2025-01-30 14:46:37 -0500

    Update revert on disconnect logic

--- src/stream.cpp
+++ src/stream.cpp
@@ -1882,15 +1882,18 @@ namespace stream {
 
       // If this is the last session, invoke the platform callbacks
       if (--running_sessions == 0) {
+        bool revert_display_config{config::video.dd.config_revert_on_disconnect};
         if (proc::proc.running()) {
 #if defined SUNSHINE_TRAY && SUNSHINE_TRAY >= 1
           system_tray::update_tray_pausing(proc::proc.get_last_run_app_name());
 #endif
-          if(config::video.dd.config_revert_on_disconnect)
-          {
-            display_device::revert_configuration();
-          }
         } else {
+          // We have no app running and also no clients anymore.
+          revert_display_config = true;
+        }
+
+        if(revert_display_config)
+        {
           display_device::revert_configuration();
         }
 
