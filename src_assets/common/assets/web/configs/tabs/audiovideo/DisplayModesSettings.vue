34e7c91129f8d0a3f0da1ab5b05859b6528d5c77
Utkarsh Dalal <utkarsh.dalal@toptal.com>
2025-02-06 09:22:58 +0530

    Update src_assets/common/assets/web/configs/tabs/audiovideo/DisplayModesSettings.vue
    added line break
        fixed some indentation
        fixed label for id
        placeholder should be the default value in most cases
        added missing closing div
    
    Co-authored-by: ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>

--- src_assets/common/assets/web/configs/tabs/audiovideo/DisplayModesSettings.vue
+++ src_assets/common/assets/web/configs/tabs/audiovideo/DisplayModesSettings.vue
@@ -17,12 +17,13 @@ const config = ref(props.config)
     <input type="number" min="1" max="3" class="form-control" id="min_fps_factor" placeholder="1" v-model="config.min_fps_factor" />
     <div class="form-text">{{ $t('config.min_fps_factor_desc') }}</div>
   </div>
-    <!--max_bitrate-->
+
+  <!--max_bitrate-->
   <div class="mb-3">
-    <label for="qp" class="form-label">{{ $t("config.max_bitrate") }}</label>
-    <input type="number" class="form-control" id="max_bitrate" placeholder="5000" v-model="config.max_bitrate"
-    />
-  <div class="form-text">{{ $t("config.max_bitrate_desc") }}</div>
+    <label for="max_bitrate" class="form-label">{{ $t("config.max_bitrate") }}</label>
+    <input type="number" class="form-control" id="max_bitrate" placeholder="0" v-model="config.max_bitrate" />
+    <div class="form-text">{{ $t("config.max_bitrate_desc") }}</div>
+  </div>
 </template>
 
 <style scoped>
