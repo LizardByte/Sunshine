8099bfbe2e6baca989c428f98c5257edcc462d81
Ishaaq <iollite@hotmail.com>
2025-06-15 13:30:04 +0100

    Add portal grab commit

--- src/platform/linux/misc.cpp
+++ src/platform/linux/misc.cpp
@@ -785,6 +785,9 @@ namespace platf {
 #ifdef SUNSHINE_BUILD_X11
       X11,  ///< X11
 #endif
+#ifdef SUNSHINE_BUILD_PORTAL
+      PORTAL,
+#endif
       MAX_FLAGS  ///< The maximum number of flags
     };
   }  // namespace source
@@ -827,6 +830,18 @@ namespace platf {
   }
 #endif
 
+#ifdef SUNSHINE_BUILD_PORTAL
+  std::vector<std::string>
+  portal_display_names();
+  std::shared_ptr<display_t>
+  portal_display(mem_type_e hwdevice_type, const std::string &display_name, const video::config_t &config);
+
+  bool
+  verify_portal() {
+    return !portal_display_names().empty();
+  }
+#endif
+
   std::vector<std::string> display_names(mem_type_e hwdevice_type) {
 #ifdef SUNSHINE_BUILD_CUDA
     // display using NvFBC only supports mem_type_e::cuda
@@ -849,6 +864,11 @@ namespace platf {
       return x11_display_names();
     }
 #endif
+#ifdef SUNSHINE_BUILD_PORTAL
+    if (sources[source::PORTAL]) {
+        return portal_display_names();
+    }
+#endif
     return {};
   }
 
@@ -862,6 +882,12 @@ namespace platf {
   }
 
   std::shared_ptr<display_t> display(mem_type_e hwdevice_type, const std::string &display_name, const video::config_t &config) {
+#ifdef SUNSHINE_BUILD_PORTAL
+    if (sources[source::PORTAL]) {
+      BOOST_LOG(info) << "Screencasting with XDG portal"sv;
+      return portal_display(hwdevice_type, display_name, config);
+    }
+#endif
 #ifdef SUNSHINE_BUILD_CUDA
     if (sources[source::NVFBC] && hwdevice_type == mem_type_e::cuda) {
       BOOST_LOG(info) << "Screencasting with NvFBC"sv;
@@ -944,6 +970,14 @@ namespace platf {
       }
     }
 #endif
+#ifdef SUNSHINE_BUILD_PORTAL
+    if (config::video.capture.empty() || config::video.capture == "portal") {
+      if (verify_portal()) {
+	sources[source::PORTAL] = true;
+      }
+    }
+#endif
+
 
     if (sources.none()) {
       BOOST_LOG(error) << "Unable to initialize capture method"sv;
