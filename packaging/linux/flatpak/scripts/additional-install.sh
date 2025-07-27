94a2a59afd070bb28c317ea89d6b948dc0611a70
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2025-07-27 10:53:59 -0400

    ci: use actionlint and problem matchers

--- packaging/linux/flatpak/scripts/additional-install.sh
+++ packaging/linux/flatpak/scripts/additional-install.sh
@@ -2,12 +2,12 @@
 
 # User Service
 mkdir -p ~/.config/systemd/user
-cp /app/share/sunshine/systemd/user/sunshine.service $HOME/.config/systemd/user/sunshine.service
-echo Sunshine User Service has been installed.
-echo Use [systemctl --user enable sunshine] once to autostart Sunshine on login.
+cp "/app/share/sunshine/systemd/user/sunshine.service" "$HOME/.config/systemd/user/sunshine.service"
+echo "Sunshine User Service has been installed."
+echo "Use [systemctl --user enable sunshine] once to autostart Sunshine on login."
 
 # Udev rule
 UDEV=$(cat /app/share/sunshine/udev/rules.d/60-sunshine.rules)
-echo Configuring mouse permission.
+echo "Configuring mouse permission."
 flatpak-spawn --host pkexec sh -c "echo '$UDEV' > /etc/udev/rules.d/60-sunshine.rules"
-echo Restart computer for mouse permission to take effect.
+echo "Restart computer for mouse permission to take effect."
