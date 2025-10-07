da79708b8d815bede901164dad605d7aa2459f6c
Pim Vermeer <pim.vermeer@gmail.com>
2025-10-07 15:51:39 +0200

    build(linux): static link std libs

--- cmake/targets/linux.cmake
+++ cmake/targets/linux.cmake
@@ -1,1 +1,4 @@
 # linux specific target definitions
+
+# Using newer c++ compilers / features on older distros causes runtime dyn link errors
+target_link_libraries(sunshine -static-libgcc -static-libstdc++)
