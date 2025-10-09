4f7cbb15f325315d75fd39adb2112c772b39346b
Pim Vermeer <pim.vermeer@gmail.com>
2025-10-09 14:42:10 +0200

    build(linux): explicit enable auto dependency for rpm package

--- cmake/packaging/linux.cmake
+++ cmake/packaging/linux.cmake
@@ -93,8 +93,9 @@ if(NOT BOOST_USE_STATIC)
                 boost-program-options >= ${Boost_VERSION}")
 endif()
 
-# This should automatically figure out dependencies on deb packages
+# This should automatically figure out dependencies on packages
 set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
+set(CPACK_RPM_PACKAGE_AUTOREQ ON)
 
 # application icon
 if(NOT ${SUNSHINE_BUILD_FLATPAK})
