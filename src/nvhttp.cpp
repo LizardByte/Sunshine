c45d586ee240c87a1c01fd6a2a61ed5561d3760e
Rohan Kapur <me@rohankapur.com>
2025-10-15 16:37:33 -0700

    Restore BacklightVersion field to serverinfo

--- src/nvhttp.cpp
+++ src/nvhttp.cpp
@@ -951,6 +951,7 @@ namespace nvhttp {
     tree.put("root.appversion", VERSION);
     tree.put("root.GfeVersion", GFE_VERSION);
     tree.put("root.FujiSunshine", "Backbone-Labs-Fork");
+    tree.put("root.BacklightVersion", "1.0");
     tree.put("root.uniqueid", http::unique_id);
     tree.put("root.HttpsPort", net::map_port(PORT_HTTPS));
     tree.put("root.ExternalPort", net::map_port(PORT_HTTP));
