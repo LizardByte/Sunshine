72cca84ceafcb3e83556465397b601eee797311c
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2026-01-24 11:44:59 -0500

    Refactor WiX packaging resources and update installer
    Replaces custom-actions.wxs and custom-shortcuts.wxs with sunshine-installer.wxs, consolidating custom actions and shortcut definitions. Adds patch.xml for WiX patching. Updates windows_wix.cmake to use new resource files, install the WiX Util extension, and adjust build directories. This streamlines and modernizes the Windows installer packaging process.

--- cmake/packaging/windows_wix.cmake
+++ cmake/packaging/windows_wix.cmake
@@ -4,8 +4,8 @@
 set(CPACK_WIX_VERSION 4)
 set(WIX_VERSION 4.0.4)
 set(WIX_UI_VERSION 4.0.4)  # extension versioning is independent of the WiX version
-set(WIX_BUILD_PARENT_DIRECTORY "${CPACK_PACKAGE_DIRECTORY}/_CPack_Packages/win64")
-set(WIX_BUILD_DIRECTORY "${WIX_BUILD_PARENT_DIRECTORY}/WIX")
+set(WIX_BUILD_PARENT_DIRECTORY "${CMAKE_BINARY_DIR}/wix_packaging")
+set(WIX_BUILD_DIRECTORY "${CPACK_PACKAGE_DIRECTORY}/_CPack_Packages/win64/WIX")
 
 # Download and install WiX tools locally in the build directory
 set(WIX_TOOL_PATH "${CMAKE_BINARY_DIR}/.wix")
@@ -38,17 +38,31 @@ if(NOT WIX_UI_INSTALL_RESULT EQUAL 0)
     message(FATAL_ERROR "Failed to install WiX UI extension, error: ${WIX_UI_INSTALL_OUTPUT}")
 endif()
 
+# Install WiX Util Extension
+execute_process(
+        COMMAND "${WIX_TOOL_PATH}/wix" extension add WixToolset.Util.wixext/${WIX_UI_VERSION}
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        ERROR_VARIABLE WIX_UTIL_INSTALL_OUTPUT
+        RESULT_VARIABLE WIX_UTIL_INSTALL_RESULT
+)
+
+if(NOT WIX_UTIL_INSTALL_RESULT EQUAL 0)
+    message(FATAL_ERROR "Failed to install WiX Util extension, error: ${WIX_UTIL_INSTALL_OUTPUT}")
+endif()
+
 # Set WiX-specific variables
 set(CPACK_WIX_ROOT "${WIX_TOOL_PATH}")
 set(CPACK_WIX_UPGRADE_GUID "512A3D1B-BE16-401B-A0D1-59BBA3942FB8")
 
-# Help/Support URLs
+# Installer metadata
 set(CPACK_WIX_HELP_LINK "https://docs.lizardbyte.dev/projects/sunshine/latest/md_docs_2getting__started.html")
+set(CPACK_WIX_PRODUCT_ICON "${SUNSHINE_ICON_PATH}")
 set(CPACK_WIX_PRODUCT_URL "${CMAKE_PROJECT_HOMEPAGE_URL}")
 set(CPACK_WIX_PROGRAM_MENU_FOLDER "LizardByte")
 
 set(CPACK_WIX_EXTENSIONS
         "WixToolset.UI.wixext"
+        "WixToolset.Util.wixext"
 )
 
 message(STATUS "cpack package directory: ${CPACK_PACKAGE_DIRECTORY}")
@@ -58,8 +72,10 @@ file(COPY "${CMAKE_CURRENT_LIST_DIR}/wix_resources/"
         DESTINATION "${WIX_BUILD_PARENT_DIRECTORY}/")
 
 set(CPACK_WIX_EXTRA_SOURCES
-        "${WIX_BUILD_PARENT_DIRECTORY}/custom-actions.wxs"
-        "${WIX_BUILD_PARENT_DIRECTORY}/custom-shortcuts.wxs"
+        "${WIX_BUILD_PARENT_DIRECTORY}/sunshine-installer.wxs"
+)
+set(CPACK_WIX_PATCH_FILE
+        "${WIX_BUILD_PARENT_DIRECTORY}/patch.xml"
 )
 
 # Copy root LICENSE and rename to have .txt extension
