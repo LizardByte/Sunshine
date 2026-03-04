998e1f62048316d73bd287df378a259fb6e49d06
Thomas Van Laere <ThomVanL@users.noreply.github.com>
2026-03-04 23:39:03 +0100

    revert(cmake): restore unix compile definitions to ebc41acf

--- cmake/compile_definitions/unix.cmake
+++ cmake/compile_definitions/unix.cmake
@@ -5,7 +5,6 @@ list(APPEND SUNSHINE_EXTERNAL_LIBRARIES
         ${CURL_LIBRARIES})
 
 # add install prefix to assets path if not already there
-# Skip prefix addition for absolute paths or development builds
-if(NOT SUNSHINE_ASSETS_DIR MATCHES "^/" AND NOT SUNSHINE_ASSETS_DIR MATCHES "^${CMAKE_INSTALL_PREFIX}")
+if(NOT SUNSHINE_ASSETS_DIR MATCHES "^${CMAKE_INSTALL_PREFIX}")
     set(SUNSHINE_ASSETS_DIR "${CMAKE_INSTALL_PREFIX}/${SUNSHINE_ASSETS_DIR}")
 endif()
