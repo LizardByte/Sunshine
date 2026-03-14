99d4e053bf6bee4f4a3dfa217b608600cf42a79c
Conn O'Griofa <connogriofa@gmail.com>
2026-03-14 17:43:17 +0000

    fix(linux/xdgportal): avoid duplicate frame insertion (#4839)

--- src/platform/linux/portalgrab.cpp
+++ src/platform/linux/portalgrab.cpp
@@ -1365,6 +1365,11 @@ namespace portal {
       return 0;
     }
 
+    // This capture method is event driven; don't insert duplicate frames
+    bool is_event_driven() override {
+      return true;
+    }
+
   private:
     bool is_buffer_redundant(const egl::img_descriptor_t *img) {
       // Check for corrupted frame
