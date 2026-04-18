62f2fffbfcfdda7139a8d1fefafabbc8ca82418a
Kishi85 <git@eightyfive.net>
2026-04-18 20:19:10 +0200

    fix(linux/kwin): Add missing advanced config UI option

--- src_assets/common/assets/web/configs/tabs/Advanced.vue
+++ src_assets/common/assets/web/configs/tabs/Advanced.vue
@@ -75,6 +75,7 @@ const config = ref(props.config)
             <option value="kms">KMS</option>
             <option value="x11">X11</option>
             <option value="portal">XDG Portal</option>
+            <option value="kwin">KWin Screencast</option>
           </template>
           <template #windows>
             <option value="ddx">Desktop Duplication API</option>
