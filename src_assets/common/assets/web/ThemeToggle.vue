ff6fd135fc8b2e1fbc8221852b86a5dd5ee39b90
Noklef <281545466+Noklef@users.noreply.github.com>
2026-05-30 17:31:43 +1000

    feat(web-ui): Created a themed drop-down
    - Updated the hover for dropdown items to actually style properly
    - Updated `ThemeToggle.vue` to implement it as a 2-column wide dropdown (left dark, right light)
    - Added translation for `theme_group_light` and `theme_group_dark`
    - Dropdown arrow rotates now + button is highlighted when open

--- src_assets/common/assets/web/ThemeToggle.vue
+++ src_assets/common/assets/web/ThemeToggle.vue
@@ -40,108 +40,80 @@ onMounted(() => {
       </span>
       <span id="bd-theme-text">{{ $t('navbar.toggle_theme') }}</span>
     </a>
-    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="bd-theme-text">
-      <li>
+    <ul class="dropdown-menu dropdown-menu-end theme-menu" aria-labelledby="bd-theme-text">
+      <li class="theme-menu-full">
         <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto" aria-pressed="true">
           <MonitorSmartphone :size="18" class="theme-icon icon"></MonitorSmartphone>
           {{ $t('navbar.theme_auto') }}
         </button>
       </li>
-      <li><hr class="dropdown-divider"></li>
       <!-- Dark Themes -->
-      <li>
+      <li class="theme-menu-group">
+        <h6 class="dropdown-header">{{ $t('navbar.theme_group_dark') }}</h6>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
           <Moon :size="18" class="theme-icon icon"></Moon>
           {{ $t('navbar.theme_dark') }}
         </button>
-      </li>
-      <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dracula" aria-pressed="false">
           <Ghost :size="18" class="theme-icon icon"></Ghost>
           {{ $t('navbar.theme_dracula') }}
         </button>
-      </li>
-      <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="slate" aria-pressed="false">
           <Layers :size="18" class="theme-icon icon"></Layers>
           {{ $t('navbar.theme_slate') }}
         </button>
-      </li>
-      <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="moonlight" aria-pressed="false">
           <CloudMoon :size="18" class="theme-icon icon"></CloudMoon>
           {{ $t('navbar.theme_moonlight') }}
         </button>
-      </li>
-      <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="midnight" aria-pressed="false">
           <CloudRain :size="18" class="theme-icon icon"></CloudRain>
           {{ $t('navbar.theme_midnight') }}
         </button>
-      </li>
-      <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="ember" aria-pressed="false">
           <Flame :size="18" class="theme-icon icon"></Flame>
           {{ $t('navbar.theme_ember') }}
         </button>
-      </li>
-      <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="nord" aria-pressed="false">
           <Mountain :size="18" class="theme-icon icon"></Mountain>
           {{ $t('navbar.theme_nord') }}
         </button>
       </li>
-      <li><hr class="dropdown-divider"></li>
       <!-- Light Themes -->
-      <li>
+      <li class="theme-menu-group">
+        <h6 class="dropdown-header">{{ $t('navbar.theme_group_light') }}</h6>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
           <Sun :size="18" class="theme-icon icon"></Sun>
           {{ $t('navbar.theme_light') }}
         </button>
-      </li>
-      <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="alucard" aria-pressed="false">
           <Droplet :size="18" class="theme-icon icon"></Droplet>
           {{ $t('navbar.theme_alucard') }}
         </button>
-      </li>
-      <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="indigo" aria-pressed="false">
           <Sparkles :size="18" class="theme-icon icon"></Sparkles>
           {{ $t('navbar.theme_indigo') }}
         </button>
-      </li>
-      <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="sunshine" aria-pressed="false">
           <Sunrise :size="18" class="theme-icon icon"></Sunrise>
           {{ $t('navbar.theme_sunshine') }}
         </button>
-      </li>
-      <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="ocean" aria-pressed="false">
           <Waves :size="18" class="theme-icon icon"></Waves>
           {{ $t('navbar.theme_ocean') }}
         </button>
-      </li>
-      <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="forest" aria-pressed="false">
           <Trees :size="18" class="theme-icon icon"></Trees>
           {{ $t('navbar.theme_forest') }}
         </button>
-      </li>
-      <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="rose" aria-pressed="false">
           <Flower :size="18" class="theme-icon icon"></Flower>
           {{ $t('navbar.theme_rose') }}
         </button>
-      </li>
-      <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="lavender" aria-pressed="false">
           <Flower2 :size="18" class="theme-icon icon"></Flower2>
           {{ $t('navbar.theme_lavender') }}
         </button>
-      </li>
-      <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="monochrome" aria-pressed="false">
           <Contrast :size="18" class="theme-icon icon"></Contrast>
           {{ $t('navbar.theme_monochrome') }}
