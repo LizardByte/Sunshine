925898435f643c42571cb2c46acb559f167611ac
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2026-06-25 10:45:48 +0200

    fix(web-ui): further simplify flatpack start command

--- packaging/linux/flatpak/scripts/sunshine.sh
+++ packaging/linux/flatpak/scripts/sunshine.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-
-PORT=47990
-
-if ! curl -k https://localhost:$PORT > /dev/null 2>&1; then
-  exec sunshine "$@"
-else
-  echo "Sunshine is already running, opening the web interface..."
-  xdg-open https://localhost:$PORT
-fi
+exec sunshine "$@"
