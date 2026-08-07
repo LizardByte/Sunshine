e9ed5188f53934fe3b8475d345e41ef94313a242
Noklef <281545466+Noklef@users.noreply.github.com>
2026-08-07 23:45:59 +1000

    fix(web-ui): Welcome page styling (#5466)

---
+++ src_assets/common/assets/web/NavbarSimple.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar navbar-sunshine">
+    <div class="container-fluid">
+      <span class="navbar-brand" title="Sunshine">
+        <img src="/images/logo-sunshine-45.png" height="45" alt="Sunshine">
+      </span>
+      <ThemeToggle/>
+    </div>
+  </nav>
+</template>
+
+<script>
+import ThemeToggle from './ThemeToggle.vue'
+
+export default {
+  components: {
+    ThemeToggle,
+  },
+}
+</script>
