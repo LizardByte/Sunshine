b4086fc8c6ba2d9f96aca74700bd57bc1db7a600
Abhishek <theabhishekroy77@gmail.com>
2026-08-24 09:01:38 +0000

    Migrate logout page

--- src_assets/common/assets/web/src/views/LogoutView.vue
+++ src_assets/common/assets/web/src/views/LogoutView.vue
@@ -1,8 +1,26 @@
-<script>
-export default {}
-</script>
-
 <template>
-  <h1>Logout</h1>
-  <p>TODO: migrate content from src/views/logout.html</p>
+  <div class="row justify-content-center py-5">
+    <div class="col-md-8 col-lg-6">
+      <div class="card">
+        <div class="card-body text-center">
+          <h1 class="h3 mb-3">{{ $t('logout.logged_out') }}</h1>
+          <p class="text-muted mb-4">{{ $t('logout.logged_out_desc') }}</p>
+          <a class="btn btn-primary" href="./">
+            <log-in :size="18" class="icon"></log-in>
+            {{ $t('logout.login') }}
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import { LogIn } from '@lucide/vue'
+
+export default {
+  components: {
+    LogIn,
+  },
+}
+</script>
