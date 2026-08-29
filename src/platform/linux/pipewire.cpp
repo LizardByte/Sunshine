0308da96275b7854f1a34748df3cec00722b9a6c
Conn O'Griofa <connogriofa@gmail.com>
2026-08-29 03:04:46 +0100

    fix(linux/pipewire): try variable rate for non-KWin compositors
    Variable rate capture works better on mutter, but since GNOME 50
    we encountered negotiation failures.
    
    It seems that if we negotiate maxFramerate with a default of variable rate
    but relaxed maximum value, the final negotiation phase actually lands on
    variable rate (after being constrained to the host refresh rate); mutter
    debug included:
    
    [2026-08-29 00:38:43.671]: Info: [pipewire] Framerate (from compositor): 0/1
    [2026-08-29 00:38:43.671]: Info: [pipewire] Framerate (from compositor, max): 60/1
    SCREEN_CAST: Video format changed to 1920x1080 (framerate: 0/1 (max: 60/1))
    SCREEN_CAST: Video format changed to 1920x1080 (framerate: 0/1 (max: 0/1))
    [2026-08-29 00:38:43.672]: Info: [pipewire] Framerate (from compositor): 0/1 (variable rate capture)
    
    Pacing does seem to be improved and Sunshine reports variable rate for the
    *final* negotiation phase.
    
    Upstream MR has been submitted, but doesn't seem strictly necessary:
    https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5278
    
    With upstream MR applied, output is more uniform:
    
    [2026-08-29 00:35:42.248]: Info: [pipewire] Framerate (from compositor): 0/1 (variable rate capture)
    [2026-08-29 00:35:42.248]: Info: [pipewire] Framerate (from compositor): 0/1 (variable rate capture)
    [2026-08-29 00:35:42.248]: Info: [pipewire] Framerate (from compositor): 0/1 (variable rate capture)
    [2026-08-29 00:35:42.248]: Info: [pipewire] Framerate (from compositor): 0/1 (variable rate capture)
    SCREEN_CAST: Video format changed to 1920x1080 (framerate: 0/1 (max: 0/1))

--- src/platform/linux/pipewire.cpp
+++ src/platform/linux/pipewire.cpp
@@ -815,10 +815,11 @@ namespace pipewire {
       // calculate frame interval we should capture at
       delay = ::video::capture_frame_interval(config);
 
-      // WORKAROUND: request variable rate (0, 1) capture only if the active compositor is KWin 5.x-6.7.x.
+      // WORKAROUND: if the active compositor is KWin, request variable rate (0, 1) capture only for versions 5.x-6.7.x.
       // Ref: https://bugs.kde.org/show_bug.cgi?id=524129
+      // Also negotiate variable rate for all other compositors. Mutter's variable rate pacing is superior.
       const static std::vector<int> kwin_version = get_running_kwin_version();
-      const static bool negotiate_variable_rate = !kwin_version.empty() && (kwin_version[0] == 5 || (kwin_version[0] == 6 && kwin_version[1] < 8));
+      const static bool negotiate_variable_rate = kwin_version.empty() || (kwin_version[0] == 5 || (kwin_version[0] == 6 && kwin_version[1] < 8));
 
       const AVRational fps = (negotiate_variable_rate ? AVRational {0, 1} : ::video::framerate_to_rational(config));
       if (fps.den != 1) {
