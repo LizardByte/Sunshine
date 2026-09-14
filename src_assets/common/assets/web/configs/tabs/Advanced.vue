b19ba1ef9f2877f5280daf9e8a63a09a25ed4408
GenshinImpactStarts <12211831@mail.sustech.edu.cn>
2026-09-14 06:22:23 +0000

    feat(web): add V4L2 encoder option

--- src_assets/common/assets/web/configs/tabs/Advanced.vue
+++ src_assets/common/assets/web/configs/tabs/Advanced.vue
@@ -105,6 +105,7 @@ const config = ref(props.config)
             <option value="nvenc">NVIDIA NVENC</option>
             <option value="vaapi">VA-API</option>
             <option value="vulkan">Vulkan</option>
+            <option value="v4l2">V4L2</option>
           </template>
           <template #macos>
             <option value="videotoolbox">VideoToolbox</option>
